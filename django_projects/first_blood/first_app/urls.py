1feef5c1f2f7b8debe9cc36704962fa9695d8399
vlad <vladyslav.lysakov@gmail.com>
2020-06-26 19:33:57 +0300

    Refactored code.

--- django_projects/first_blood/first_app/urls.py
+++ django_projects/first_blood/first_app/urls.py
@@ -19,9 +19,15 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, reverse_lazy
 
 from .views import (
-    RegistrationUser, main_page, info_about_user, AddArticle,
-    DisplayAllArticles, DisplayArticlesByUser,
-    UserInfoUpdate, post_edit_view, CustomLoginView,
+    RegistrationUser,
+    main_page,
+    info_about_user,
+    AddArticle,
+    DisplayAllArticles,
+    DisplayArticlesByUser,
+    UserInfoUpdate,
+    post_edit_view,
+    CustomLoginView,
     CheckPinView,
 )
 
@@ -33,9 +39,11 @@ urlpatterns = [
     path('login/code-authenticate/', decorators.axes_dispatch(CheckPinView.as_view()), name='verify'),
     path('blog/user/add_article/', AddArticle.as_view(), name='add_article'),
     path('blog/user/edit/', UserInfoUpdate.as_view(), name='edit_info'),
-    path('blog/user/change_password/', auth_views.PasswordChangeView.as_view(
-        template_name='info/ch_psswd.html',
-        success_url=reverse_lazy('main_page')), name='change-password'),
+    path('blog/user/change_password/',
+         auth_views.PasswordChangeView.as_view(
+             template_name='info/ch_psswd.html',
+             success_url=reverse_lazy('main_page')),
+         name='change-password'),
     path('blog/user/<str:user_articles>/', DisplayArticlesByUser.as_view(), name='user_articles'),
     path('blog/<str:name>/', DisplayAllArticles.as_view(), name='specific_article'),
     path('blog/user/edit_article/<int:pk>', post_edit_view, name='edit_article'),
--- django_projects/first_blood/first_blood/urls.py
+++ django_projects/first_blood/first_blood/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from two_factor.urls import urlpatterns as tf_urls
-#from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('first_app.urls')),
     path('', include('django.contrib.auth.urls')),
-   # path('', include(tf_urls)),
-    #path('', include(tf_twilio_urls)),
 ]
